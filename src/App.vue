c7b378fe0423a14d1b056baf6f7b8d5a9ab70ecd
jose ariza <l.jlac001@gmail.com>
2021-03-05 15:29:02 -0500

    implements base movies list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Git bisect test</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/440px-Studio_Ghibli_logo.svg.png">
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-md-6">
+        <MoviesList/>
+      </div>
+      <div class="col-md-6">
+        asdsad
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MoviesList from './components/MoviesList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MoviesList
   }
 }
 </script>
---
+++ src/components/MoviesList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <div>
+      <ul>
+        <li v-for="movie in moviesList" :key="movie.id">
+          <a>{{ movie.title }} ({{ movie.original_title }})</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'MoviesList',
+  props: {
+  },
+  data () {
+    return {
+      moviesList: []
+    }
+  },
+  mounted () {
+    axios
+      .get('https://ghibliapi.herokuapp.com/films/')
+      .then(response => (this.moviesList = response.data))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
